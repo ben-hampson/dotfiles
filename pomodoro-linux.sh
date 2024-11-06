d962204e21307985dc4e79c25ffdffba4aa0acc4
Ben Hampson <email@email.com>
2024-11-06 12:27:19 +0100

    improvements to pomodoro-linux.sh

--- pomodoro-linux.sh
+++ pomodoro-linux.sh
@@ -8,10 +8,15 @@ pomo_options["break"]="10"
 
 pomodoro () {
   if [ -n "$1" -a -n "${pomo_options["$1"]}" ]; then
-  val=$1
-  echo $val | lolcat
-  timer ${pomo_options["$val"]}m
-  spd-say "'$val' session done"
+    val=$1
+    echo $val | lolcat
+    timer ${pomo_options["$val"]}m
+    spd-say "'$val' session done"
+    date
+  else
+    timer "${1}m"
+    spd-say "Session done"
+    date
   fi
 }
 
