3cb9e1aa3a81c94996b2a9eea2c262c9eeac376a
Ben Hampson <email@email.com>
2025-03-11 17:14:14 +0100

    add to fedora setup

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -6,10 +6,19 @@ sudo dnf install -y \
   espanso-wayland \
   fzf \
   1password \ # needs repos 
-  translate-shell
+  translate-shell \
+  lolcat \
+  python3-pip \
+  mutt
 
 brew install \
   argocd \
-  openshift-cli
+  openshift-cli \
+  caarlos0/tap/timer
 
 curl -s https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh && sudo chmod +x /usr/local/bin/cht.sh
+
+pip install shell-gpt
+
+# Hyprland
+sudo dnf install -y rofi waybar
