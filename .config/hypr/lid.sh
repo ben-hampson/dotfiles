e0e5a989f8b8803e2668d9f1ba5f2b98827bc6a3
Ben Hampson <email@email.com>
2025-04-10 19:43:02 +0200

    fix: when closing lid and no external monitor, suspend

--- .config/hypr/lid.sh
+++ .config/hypr/lid.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env zsh
 
 if [[ "$(hyprctl monitors)" =~ "\sDP-[0-9]+" ]]; then
+  echo "External monitor plugged in."
+
   if [[ $1 == "open" ]]; then
     echo "Opened lid. Enabled laptop screen."
     hyprctl keyword monitor "eDP-1,preferred,auto,auto"
   else
+    echo "External monitor present. Closed lid. Disabling laptop screen so workspace 1 doesn't stay on it."
     hyprctl keyword monitor "eDP-1,disable"
-    echo "Closed lid. Disabled laptop screen so workspace 1 doesn't stay on it."
   fi
+else
+  echo "No external monitor plugged in. Closed lid, suspending laptop."
+  hyprlock & disown && systemctl suspend
 fi
