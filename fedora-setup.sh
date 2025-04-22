3326efa79a7c0c522e9e83f784e48f003675c819
Ben Hampson <email@email.com>
2025-04-22 09:42:36 +0200

    install timeshift, install 1password correctly

--- fedora-setup.sh
+++ fedora-setup.sh
@@ -15,14 +15,19 @@ sudo dnf install -y \
   speedtest-cli \
   btop \
   fzf \
-  1password \ # needs repos 
   translate-shell \
   lolcat \
   python3-pip \
   mutt \
   yq \
   kubectl \
-  pipx
+  pipx \
+  timeshift
+
+# 1Password
+sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
+sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
+sudo dnf install 1password
 
 brew install \
   argocd \
