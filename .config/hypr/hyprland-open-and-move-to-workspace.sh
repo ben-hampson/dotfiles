db175faec4597b98e8f7b6325384589a185b7cc2
Ben Hampson <email@email.com>
2025-07-07 08:51:51 +0200

    add move-to-workspace script

---
+++ .config/hypr/hyprland-open-and-move-to-workspace.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Open application, wait two seconds, and move the application to another workspace.
+
+EXEC_COMMAND=$1
+INITIAL_CLASS=$2
+WORKSPACE=$3
+
+
+hyprctl dispatch exec "$EXEC_COMMAND"
+sleep 2
+hyprctl clients -j
+
+# Get this to work with $INITIAL_CLASS
+pid=$(hyprctl clients -j | jq ".[] | select(.initialClass == ${INITIAL_CLASS}) | .pid")
+
+echo $pid
+hyprctl dispatch movetoworkspacesilent ${WORKSPACE},pid:${pid}
